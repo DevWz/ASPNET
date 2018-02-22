b21852f481b6c1d4136b894cf7b6d769b0032589
Wellington Antonio Zaneze <wellingtonantonio_zaneze@live.com>
2018-02-22 18:29:25 -0300

    AspNet.Debug project updated

--- AspNet.Debug/Program.cs
+++ AspNet.Debug/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNet.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,109 @@ namespace AspNet.Debug
     {
         static void Main(string[] args)
         {
+
+            AspNet.Core.Models.DB.AspNetDbContext manager = new Core.Models.DB.AspNetDbContext();
+
+            // OK
+            /*
+            Cliente cliente = new Cliente()
+            {
+                Email = "mail@mail.com",
+                Senha = "123"
+            };
+            */
+
+            // EXCEPTION // Cliente obrigatorio para Dado
+            /*
+            Dado dado = new Dado()
+            {
+                Nome = "DevWz",
+                CPF = "00000000000"
+            };
+            */
+
+            // OK
+            /*
+            Cliente cliente = new Cliente()
+            {
+                Email = "mail@mail.com",
+                Senha = "123",
+                Dado = new Dado()
+                {
+                    Nome = "DevWz",
+                    CPF = "00000000000"
+                }
+            };
+            */
+
+            // OK
+            /*
+            Dado dado = new Dado()
+            {
+                Cliente = manager.Clientes.SingleOrDefault(x => x.Id.Equals(1)),
+                Nome = "DevWz",
+                CPF = "000000000000"
+            };
+            */
+
+            /*
+            Dado dado = new Dado()
+            {
+                Cliente = new Cliente()
+                {
+                    Email = "mail@mail.com",
+                    Senha = "123"
+                },
+                Nome = "DevWz",
+                CPF = "00000000000"
+            };
+            */
+
+            // EXCEPTION // Conflito de propriedades
+            /*
+            Dado dado = new Dado()
+            {
+                Cliente = new Cliente()
+                {
+                    Email = "mail@mail.com",
+                    Senha = "123",
+                    Dado = new Dado()
+                    {
+                        Nome = "AAAA",
+                        CPF = "11111111111"
+                    }
+                },
+                Nome = "DevWz",
+                CPF = "00000000000"
+            };
+            */
+
+            // OK
+            /*
+            Cliente cliente = new Cliente()
+            {
+                Email = "mail@mail.com",
+                Senha = "123",
+                Fones = new List<Fone>()
+                {
+                    new Fone() { N = "00000000" },
+                    new Fone() { N = "11111111" }
+                },
+                Dado = new Dado()
+                {
+                    CPF = "00000000000",
+                    Nome = "DevWz"
+                }
+            };
+            */
+
+            Cliente cliente = manager.Clientes.Single(x => x.Id.Equals(1));
+            manager.Clientes.Remove(cliente);
+
+            // manager.Clientes.Add(cliente);
+            // manager.Dados.Add(dado);
+            manager.SaveChanges();
+
         }
     }
 }
